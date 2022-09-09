587fdc4d2e4446563bc034e45a089886ef947456
Josh Watson <josh@legendarylion.com>
2022-09-09 19:36:49 -0400

    Updated to allow for args for type and class

--- template-parts/header-logo.php
+++ template-parts/header-logo.php
@@ -1,10 +1,14 @@
 <?php 
     $logo = legendary_toolkit_get_theme_option('logo');
+    if (array_key_exists('type', $args) && $args['type'] == 'scrolling') {
+        $logo = legendary_toolkit_get_theme_option('scrolling_logo');
+    }
     $logo_url = ($logo) ? esc_url(wp_get_attachment_image_url($logo, 'medium')) : '';
-    $id = $args['id'];
+    $id = array_key_exists('id', $args) ? $args['id'] : '';
+    $class = (array_key_exists('class', $args)) ? $args['class'] : '';
     if ( $logo ): ?>
         <a href="<?=esc_url( home_url( '/' )); ?>">
-            <img id="<?=$id;?>" src="<?=$logo_url; ?>" alt="<?=esc_attr( get_bloginfo( 'name' ) ); ?>">
+            <img class="<?=$class;?>" id="<?=$id;?>" src="<?=$logo_url; ?>" alt="<?=esc_attr( get_bloginfo( 'name' ) ); ?>">
         </a>
     <?php else : ?>
         <a class="site-title" href="<?=esc_url( home_url( '/' )); ?>"><?php esc_url(bloginfo('name')); ?></a>
